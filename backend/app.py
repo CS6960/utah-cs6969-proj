7e8dcc14056ca5c778f94d5bf84ac3d841d64e0f
Ty Tsai <tsaizhihao@gmail.com>
2026-02-25 15:37:32 -0700

    Update allow origins to avoid api abuse

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://cs6960.github.io/utah-cs6969-proj/", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
